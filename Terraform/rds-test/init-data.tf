6aa1e58024704c9976299ae83bbc88cca4e73ec1
Anhgrew <socksockmoizui@gmail.com>
2023-04-25 13:49:06 +0700

    Update init data resource

--- Terraform/rds-test/init-data.tf
+++ Terraform/rds-test/init-data.tf
@@ -1,4 +1,4 @@
-resource "null_resource" "init_db" {
+resource "null_resource" "db_script" {
   depends_on = [module.elearning-rds] # Wait for the RDS instance to be created before running the local-exec provisioner
 
   connection {
@@ -6,10 +6,21 @@ resource "null_resource" "init_db" {
     user     = module.elearning-rds.rds_prod_user
     password = var.PASSWORD
     port     = module.elearning-rds.rds_prod_port
+    # Add any additional connection parameters if needed, such as SSL options
   }
 
   provisioner "local-exec" {
-    command = "mysql -h ${module.elearning-rds.rds_prod_endpoint} -u ${module.elearning-rds.rds_prod_user} -p'${var.PASSWORD}' -P ${module.elearning-rds.rds_prod_port} -e 'CREATE DATABASE IF NOT EXISTS elearning;' < db_backup.sql"
-
+    command = <<-EOT
+      mysql -h "${module.elearning-rds.rds_prod_endpoint}" \
+            -u "${module.elearning-rds.rds_prod_user}" \
+            -p"${var.PASSWORD}" \
+            -P "${module.elearning-rds.rds_prod_port}" \
+            -e 'CREATE DATABASE IF NOT EXISTS elearning;' \
+            && mysql -h "${module.elearning-rds.rds_prod_endpoint}" \
+               -u "${module.elearning-rds.rds_prod_user}" \
+               -p"${var.PASSWORD}" \
+               -P "${module.elearning-rds.rds_prod_port}" \
+               elearning < "${path.module}/db_backup.sql"
+    EOT
   }
 }
